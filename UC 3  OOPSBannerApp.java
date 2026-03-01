1eb2b0bafe893134b5d8c2bfa56c59bd0f390f73
diyap00712-web <diyap00712@gmail.com>
2026-03-01 12:33:31 +0530

    Add files via upload

---
+++ UC 3  OOPSBannerApp.java
@@ -0,0 +1,17 @@
+public class OOPSBannerUC3 {
+
+    public static void main(String[] args) {
+
+        String line1 = String.join("   ", "OOOO", "O  O", "PPPP", "SSSS");
+        String line2 = String.join("   ", "O  O", "O  O", "P  P", "S   ");
+        String line3 = String.join("   ", "O  O", "O  O", "PPPP", "SSSS");
+        String line4 = String.join("   ", "O  O", "O  O", "P   ", "   S");
+        String line5 = String.join("   ", "OOOO", "O  O", "P   ", "SSSS");
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+    }
+}
